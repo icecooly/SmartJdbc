92d327d2795846d41c0d4501a2e7e011936bb25f
icecooly <icecooly.du@qq.com>
2020-04-13 09:50:00 +0800

    add

--- src/main/java/io/itit/smartjdbc/enums/SqlOperator.java
+++ src/main/java/io/itit/smartjdbc/enums/SqlOperator.java
@@ -18,12 +18,12 @@ public enum SqlOperator {
     NOT_LIKE_LEFT,
     LIKE_RIGHT,
     NOT_LIKE_RIGHT,
-    EQ,
-    NE,
-    GT,
-    GE,
-    LT,
-    LE,
+    EQ,//等于
+    NE,//不等于
+    GT,//大于
+    GE,//大于等于
+    LT,//小于
+    LE,//小于等于
     IS_NULL,
     IS_NOT_NULL,
     BETWEEN;
